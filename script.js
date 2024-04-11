6b60ec1e0deff32c59c62383c7bd9e42ff0e2ef1
Shivam kumar <49615953+shivamjamshedpur@users.noreply.github.com>
2024-04-11 21:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
-  // your code here
+  // your code her
+  if (!str || str.length === 0) {
+    return [];
+  }
+  
+  const chunks = [];
+  
+  for (let i = 0; i < str.length; i += chunkLength) {
+    chunks.push(str.slice(i, i + chunkLength));
 }
 
 // Do not change the code below
